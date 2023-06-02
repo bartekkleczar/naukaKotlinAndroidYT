8e019316c54561a2a47362b31fbbe069b8d0b95a
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-02 18:18:10 +0200

    modifiers for width and height

--- app/src/main/java/pl/klenczi/drugifilmik/MainActivity.kt
+++ app/src/main/java/pl/klenczi/drugifilmik/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.klenczi.drugifilmik.ui.theme.DrugiFilmikTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,13 +28,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Row(modifier = Modifier
-                    .fillMaxSize()
+                    //.fillMaxSize(0.5f)
+                     .fillMaxHeight(0.5f) .fillMaxWidth(0.7f)
+                    //.width(200.dp) .height(300.dp)
                     .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically, // centruje elementy z kolumny w wyznaczonym obszarze
+                verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceAround
-                    // .SpaceBetween - rozdziela elementy na poziome granice
-                    // .SpaceEvenly - dzieli na rowne czesc i daje elementy na granicach tych czesci
-                    // .SpaceAround - przestrzen miedzy lementami jest taka sama ale miedzy elementem a granica polowe mniejsza
             ){
                 Text("Hello")
                 Text("World")
