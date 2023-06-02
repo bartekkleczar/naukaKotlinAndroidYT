cf5990943f32b463df45b2263e56f235296266d4
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-02 19:26:37 +0200

    wlasciwosci wykonuja sie jedna po drugiej - demonstracja tego

--- app/src/main/java/pl/klenczi/trzecifilmik/MainActivity.kt
+++ app/src/main/java/pl/klenczi/trzecifilmik/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -30,12 +32,23 @@ class MainActivity : ComponentActivity() {
             Column(modifier = Modifier
                 .background(Color.Green)
                 .fillMaxHeight(0.5f)
+                .fillMaxWidth()
                 .width(600.dp)
                 //.requiredWidth(600.dp) -> wywala poza ekran jak sie da za duza
-                .padding(top = 15.dp)
+                .border(5.dp, Color.Magenta)
+                .padding(5.dp)
+                .border(5.dp, Color.Blue)
+                .padding(5.dp)
+                .border(10.dp, Color.Red)
+                .padding(10.dp)
             ) {
                 Text("Hello", modifier = Modifier
-                    .offset(0.dp, 20.dp)) // działa jak margin ale pozwala na nakładanie się elementów
+                    .border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .offset(20.dp, 20.dp)
+                    .border(10.dp, Color.Black)
+                    .padding(10.dp)
+                )
                 Spacer(modifier = Modifier.height(50.dp)) // pusty kontener
                 Text("World")
             }
