bd3b4afcddb3f0b951ccaae57b4d20e8ad2b1433
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-03 13:40:26 +0200

    stylowanie konkretnych czesci stringa

--- app/src/main/java/pl/klenczi/jcomposestylingtext/MainActivity.kt
+++ app/src/main/java/pl/klenczi/jcomposestylingtext/MainActivity.kt
@@ -12,12 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import pl.klenczi.jcomposestylingtext.ui.theme.JComposeStylingTextTheme
@@ -35,12 +38,32 @@ class MainActivity : ComponentActivity() {
             Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
         )
         setContent {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF101010))
-            ){
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
                 Text(
-                    text = "Jetpack Compose",
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
                     color = Color.White,
                     fontSize = 30.sp,
                     fontFamily = fontFamily,
