c07b70f7b0de6861085120fbff4a3c6662731ec8
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-05 17:32:46 +0200

    animacja z infinieTransition dla koloru +  tween()

--- app/src/main/java/pl/klenczi/jcomposeanimations/MainActivity.kt
+++ app/src/main/java/pl/klenczi/jcomposeanimations/MainActivity.kt
@@ -3,12 +3,16 @@ package pl.klenczi.jcomposeanimations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
@@ -30,16 +34,25 @@ class MainActivity : ComponentActivity() {
             var sizeState by remember{ mutableStateOf(200.dp) }
             val size by animateDpAsState(
                 targetValue = sizeState,
-                keyframes {
-                    durationMillis = 5000
-                    sizeState at 0 with LinearEasing
-                    sizeState *1.5f at 1000 with FastOutLinearInEasing
-                    sizeState * 2f at 5000
-                }
+                tween(
+                    durationMillis = 1000
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(
+                        durationMillis = 2000
+                    ),
+                        repeatMode = RepeatMode.Reverse
+
+                )
             )
             Box(modifier = Modifier
                 .size(size)
-                .background(Color.Red),
+                .background(color),
                 contentAlignment = Alignment.Center
             )
             {
