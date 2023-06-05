5eedefc3db2dd1e12ec2e9633cc138ad767a4a58
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-06-05 17:02:10 +0200

    initial

---
+++ app/src/main/java/pl/klenczi/jcomposeanimations/MainActivity.kt
@@ -0,0 +1,24 @@
+package pl.klenczi.jcomposeanimations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
