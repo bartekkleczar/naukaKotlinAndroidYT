769539a81b5a41820ca6b26f20d033687a824780
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-08-15 20:35:05 +0200

    done

--- top-app-bar/app/src/main/java/com/example/topappbar/MainActivity.kt
+++ top-app-bar/app/src/main/java/com/example/topappbar/MainActivity.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.topappbar.ui.theme.TopAppBarTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +39,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val scrollBehavior = TopAppBarDefaults
+                        //.pinnedScrollBehavior() // cały czas widać
+                        //.enterAlwaysScrollBehavior() // w dół -> zwija się; w górę -> od razu się pokazuje
+                        .exitUntilCollapsedScrollBehavior() //w dół i w górę -> od razu zwija/rozwija się
+                    Scaffold (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        topBar = {
+                            TopAppBar/*CenterAlignedTopAppBar; MediumTopAppBar; LargeTopAppBar*/(
+                                title = {
+                                    Text(text = "My Notes")
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.ArrowBack,
+                                            contentDescription = "Go back"
+                                        )
+                                    }
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.FavoriteBorder,
+                                            contentDescription = "Mark as favourite"
+                                        )
+                                    }
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Edit,
+                                            contentDescription = "Edit note"
+                                        )
+                                    }
+                                },
+                                scrollBehavior = scrollBehavior
+                            )
+                        }
+                    ){values ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(values)
+                        ){
+                            items(100){
+                                Text(
+                                    text = "Item $it",
+                                    Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
