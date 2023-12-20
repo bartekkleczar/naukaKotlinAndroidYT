fe2221da80e499f27cf464ee8a2281aa3f08daee
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2023-12-20 15:25:55 +0100

    button contrloed sheet

--- bottom-sheets/app/src/main/java/com/example/bottomsheets/MainActivity.kt
+++ bottom-sheets/app/src/main/java/com/example/bottomsheets/MainActivity.kt
@@ -3,44 +3,61 @@ package com.example.bottomsheets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.bottomsheets.ui.theme.BottomSheetsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BottomSheetsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val sheetState = rememberModalBottomSheetState()
+                    var isSheetOpened by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TextButton(onClick = {
+                            isSheetOpened = true
+                        }) {
+                            Text(text = "Open sheet")
+                        }
+                    }
+
+                    if(isSheetOpened){
+                        ModalBottomSheet(
+                            sheetState = sheetState,
+                            onDismissRequest = { isSheetOpened = false }
+                        ) {
+                            Checkbox(checked = false, onCheckedChange = {})
+                            Text(text = "smth")
+                        }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BottomSheetsTheme {
-        Greeting("Android")
     }
 }
